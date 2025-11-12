df766873aa25667c2232526d4459bf6d3055c366
pawarshivamd <pawarshivam8055@gmail.com>
2025-11-12 23:56:33 +0530

    less code

---
+++ normal/Arrays and Loops/index.js
@@ -0,0 +1,9 @@
+// Arrays and Loops
+// Create an Array containing 5 different fruit names.
+//  Now, use a for loop to print each fruit name from the array to the console.
+
+const fruit = ["mango","apple","banana","peru","sitafal"]
+
+for(let i = 0; i < fruit.length; i++){
+    console.log(fruit[i])
+}
---
+++ normal/funtion.js
@@ -0,0 +1,11 @@
+// Create a function named calculateArea. This function should accept two arguments: 
+// length and width. The function must multiply these two values
+//  and return the resulting Area.
+
+const calculateArea = (length,width) =>{
+    return length * width
+}
+console.log(calculateArea(3,6))
+
+
+// Arrays and Loops
