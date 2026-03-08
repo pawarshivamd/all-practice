2e7e331d26564cff8a488976129a848fd885d555
pawarshivamd <pawarshivam8055@gmail.com>
2026-03-08 14:52:15 +0530

    auth whit jwt is compleat

--- auth-shrey/src/app.js
+++ auth-shrey/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const authRoutes = require("./routes/user.route")
+const cookieParser = require('cookie-parser')
 const app = express()
-app.use(express.json())
 
-app.use('/auth',authRoutes)
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/auth', authRoutes)
 
 module.exports = app
--- auth-shrey/src/routes/user.route.js
+++ auth-shrey/src/routes/user.route.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const jwt = require('jsonwebtoken');
 const userModel = require('../models/user.model')
 
 const routes = express.Router()
@@ -10,7 +11,8 @@ routes.post('/register', async (req, res) => {
     const user = await userModel.create({
         username, password
     })
-
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+    res.cookie("token", token)
     res.status(201).json({
         message: "user register successfully",
         user
@@ -21,25 +23,59 @@ routes.get('/login', async (req, res) => {
     const { username, password } = req.body
 
     const isUserAvailable = await userModel.findOne({
-        username : username
+        username: username
     })
 
-    if(!isUserAvailable) {
+    if (!isUserAvailable) {
         return res.status(401).json({
-            message:"user account not found"
+            message: "user account not found"
         })
     }
 
-    const isPasswordValid  = password == isUserAvailable.password
+    const isPasswordValid = password == isUserAvailable.password
 
-    if(!isPasswordValid){
+    if (!isPasswordValid) {
         return res.status(401).json({
-            message:"password is wrong"
+            message: "password is wrong"
         })
     }
 
     res.status(201).json({
-        message:"user login successfully"
+        message: "user login successfully"
+    })
+})
+
+routes.get('/user', async (req, res) => {
+    const { token } = req.cookies
+
+    if (!token) {
+        return res.status(401).json({
+            message: "unauthorized"
+        })
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await userModel.findOne({
+            _id: decoded.id
+        }).select("-password -__v")
+
+        res.status(200).json({
+            message: "user data fetched successfully",
+            user
+        })
+    } catch (err) {
+        return res.status(404).json({
+            message: "invalid token"
+        })
+    }
+
+})
+
+routes.get('/logout', async (req, res) => {
+    res.clearCookie('token')
+    res.status(200).json({
+        message:"user logout successfully"
     })
 })
 
